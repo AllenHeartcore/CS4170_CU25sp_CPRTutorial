1e1b676a62ff0e182825db8c3235f85fde975829
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-04-22 02:09:14 -0400

    [Feature/steps] Draft learning phase layout
    Also fix Bootstrap grid style in homepage

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, jsonify, Response
 
+import json
+from pathlib import Path
+
 
 # Bookkeeping
 
 
+read_json = lambda x: json.loads(Path(x).read_text(encoding="utf-8"))
+dict2list = lambda x: list(x.items())
+
+cpr_steps = dict2list(read_json("cpr_steps.json"))
 app = Flask(__name__)
 
 
@@ -13,6 +20,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/steps/<id>")
+def steps(id):
+    name, details = cpr_steps[int(id) - 1]
+    return render_template(
+        "steps.html",
+        id=id,
+        name=name,
+        details=details,
+    )
 
 
 if __name__ == "__main__":
