f623c919f178b27ae1995e662c6b1b6782a37f64
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-04-29 00:33:47 -0400

    [Feature/rhythm] Add diff%-based reset trigger
    Right now this is over-smoothed by the window size of 16, yet this already feels unstable...

--- static/js/rhythm.js
+++ static/js/rhythm.js
@@ -1,19 +1,40 @@
 $(document).ready(function () {
     const tapButton = document.getElementById("tapButton");
     const bpmDisplay = document.getElementById("bpmDisplay");
-    let taps = [];
+
+    // if new BPM is outside +-25% of the previous value, reset algorithm
+    const RESET_THRES = 0.25;
+    const QUEUE_SIZE = 16; // larger = smoother
+    let lastBPM = null;
+    let taps = Array(QUEUE_SIZE).fill(null);
 
     tapButton.addEventListener("click", () => {
         const now = Date.now();
+        taps.shift();
         taps.push(now);
-        taps = taps.filter((time) => now - time <= 10000);
 
-        if (taps.length > 1) {
-            const interval = (taps[taps.length - 1] - taps[0]) / 1000;
-            const bpm = ((taps.length - 1) / interval) * 60;
-            bpmDisplay.textContent = Math.round(bpm) + " BPM";
-        } else {
-            bpmDisplay.textContent = "Tap more!";
+        let validTaps = taps.filter((time) => time !== null);
+        let validLength = validTaps.length;
+        let BPM = 0;
+
+        if (validTaps.length > 1) {
+            BPM =
+                ((validLength - 1) * 60000) /
+                (validTaps[validLength - 1] - validTaps[0]);
+            lastBPM = lastBPM || BPM;
+
+            if (
+                lastBPM !== null &&
+                Math.abs(BPM - lastBPM) > lastBPM * RESET_THRES
+            ) {
+                // reset algorithm
+                BPM = 0;
+                lastBPM = null;
+                taps.fill(null);
+                taps[QUEUE_SIZE - 1] = now;
+            }
         }
+
+        bpmDisplay.textContent = BPM.toFixed(3);
     });
 });
